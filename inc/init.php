13a90cbc8c6d69ed8aadfb630bfc70996af12c63
darto <darto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-05 12:20:42 +0000

    update to version 1.2 add default class for panel settings and custom css
    git-svn-id: https://plugins.svn.wordpress.org/arabic-font/tags/1.2@1060359 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/init.php
@@ -0,0 +1,49 @@
+<?php
+
+/* ----------------------------------------------------------
+Declare vars
+------------------------------------------------------------- */
+$AF_pluginname = "Arabic Fonts";
+$shortname = "AF";
+
+/*---------------------------------------------------
+register settings
+----------------------------------------------------*/
+function AF_settings_init(){
+register_setting( 'AF_settings', 'AF_settings' );
+wp_enqueue_style("AF_panel_style", plugins_url("arabic-font/css/panel.css"), false, "1.0", "all");
+}
+
+/*---------------------------------------------------
+add settings page to menu
+----------------------------------------------------*/
+function AF_add_settings_page() {
+add_menu_page('Arabic Fonts', 'Arabic Font', 'administrator', __FILE__, 'AF_settings_page','dashicons-editor-textcolor', 100);
+}
+
+function AF_check_clean_installation(){
+	add_action( 'admin_notices', 'AF_reminder' );
+}
+
+if ( ! function_exists( 'AF_reminder' ) ){
+	function AF_reminder(){
+		global $shortname, $AF_pluginname, $current_screen;
+
+		if ( false === get_option( 'AF_fontfamily' ) && 'appearance_page_core_functions' != $current_screen->id ){
+			printf( __('<div class="updated"><p>This is a fresh installation of Arabic Font. Don\'t forget to go to <a href="'.admin_url( "admin.php?page=arabic-font/inc/init.php" ).'">Arabic Font</a> to set it up. This message will disappear once you have clicked the Save button within the <a href="'.admin_url( "admin.php?page=arabic-font/inc/init.php" ).'">plugin\'s options page</a>.</p></div>',$AF_pluginname));
+		}
+	}
+}
+/*
+function AF_admin_script() {
+echo '';
+}
+*/
+
+/*---------------------------------------------------
+add actions
+----------------------------------------------------*/
+add_action( 'admin_init', 'AF_settings_init' );
+add_action( 'admin_menu', 'AF_add_settings_page' );
+add_action( 'admin_init', 'AF_check_clean_installation' );
+//add_action('admin_head', 'AF_admin_script');
---
+++ inc/option.php
@@ -0,0 +1,59 @@
+<?php
+/* ---------------------------------------------------------
+Declare options
+----------------------------------------------------------- */
+ 
+$AF_options = array (
+ 
+array( "name" => $AF_pluginname." Options",
+"type" => "title"),
+
+/* ---------------------------------------------------------
+section
+----------------------------------------------------------- */
+array( "name" => "Settings",
+"type" => "section",
+"class" => "dashicons-editor-paragraph"),
+array( "type" => "open"),
+
+array( "name" => "Font Family",
+"desc" => "Choose one of your main Arabic fonts",
+"id" => $shortname."_fontfamily",
+"type" => "select",
+"options" => array("flatjozoor" => "JF Flat Jozoor", "arajozoor" => "Ara Jozoor", "kfgqpc" => "KFGQPC Uthman Taha Naskh", "pdms" => "PDMS Saleem QuranFont", "amiriweb" => "AmiriWeb", "amiriquranweb" => "Amiri QuranWeb", "droidarabicnaskh" => "Droid Arabic Naskh", "droidarabickufi" => "Droid Arabic Kufi", "thabit" => "Thabit", "scheherazade" => "Scheherazade"),
+"std" => "KFGQPC Uthman Taha Naskh"),
+ 
+array( "name" => "Font Size",
+"desc" => "Default font size",
+"id" => $shortname."_fontsize",
+"type" => "text",
+"std" => "18"),
+
+array( "name" => "Line Height",
+"desc" => "Default line height",
+"id" => $shortname."_lineheight",
+"type" => "text",
+"std" => "45"),
+
+array( "name" => "Text Align",
+"desc" => "Default text align",
+"id" => $shortname."_textalign",
+"type" => "select",
+"options" => array("center" => "Center", "right" => "Right", "left" => "Left"),
+"std" => "Center"),
+
+array( "name" => "Default class",
+"desc" => "Default class based on panel settings. If you want to add multiple class separated by comma, like: .arabic, .arab, .textarab",
+"id" => $shortname."_defaultcssclass",
+"type" => "text",
+"std" => ".arab"),
+
+array( "name" => "Custom Styles",
+"desc" => "Custom CSS you want to add for some additional need.",
+"id" => $shortname."_customcss",
+"type" => "textarea",
+"std" => ""),
+
+array( "type" => "close"),
+ 
+);
